9cdb11689d26ed765d608cd70843a58b92a61350
Tyler Porter <hello@cleverladder.com>
2024-10-19 13:33:05 -0700

    Tidy up

--- src/routes/Reviews.svelte
+++ src/routes/Reviews.svelte
@@ -24,6 +24,33 @@
 
 		<div class="reviewsWrap">
 			<div>
+				<div class="stars">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+						/></svg
+					>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+						/></svg
+					>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+						/></svg
+					>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+						/></svg
+					>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+							d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+						/></svg
+					>
+				</div>
 				<div class="reviews">
 					<swiper-container
 						autoplay-delay="3000"
@@ -48,6 +75,18 @@
 			<div class="image">
 				<img src="images/reviews.jpg" alt="Vehicle repaired by McClinton Auto Collision" />
 			</div>
+			<a
+				href="https://www.carwise.com/auto-body-shops/mcclinton-auto-collision-inc-albany-or-97322/555006?w=McClinton+Auto+Collision+Inc."
+				target="_BLANK"
+				class="button"
+			>
+				<span>Read More</span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+					><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"
+					/></svg
+				>
+			</a>
 		</div>
 	</div>
 </section>
@@ -72,7 +111,7 @@
 
 	.reviewsWrap > div:first-child {
 		overflow: hidden;
-		padding: 50px 50px 0;
+		padding: 50px;
 	}
 
 	.container {
@@ -87,6 +126,7 @@
 	}
 
 	.reviewsWrap {
+		position: relative;
 		display: grid;
 		grid-template-columns: 1fr 300px;
 		background: white;
@@ -95,6 +135,16 @@
 	}
 	.reviews p {
 		font-size: 1.3rem;
+	}
+	.stars {
+		display: flex;
+		gap: 10px;
+	}
+	.stars svg {
+		width: 20px;
+	}
+	.stars path {
+		fill: var(--green);
 	}
 	p.name {
 		font-weight: bold;
@@ -108,13 +158,34 @@
 	}
 	.image img {
 		width: 100%;
+		height: 100%;
 		display: block;
 		aspect-ratio: 1/1;
 		object-fit: cover;
-		object-fit: center;
+		object-position: center;
 	}
 	.swiper-initialized {
 		display: none;
+	}
+
+	.button {
+		position: absolute;
+		bottom: -22px;
+		left: 50px;
+		display: flex;
+		gap: 8px;
+	}
+
+	.button:hover {
+		background: var(--black);
+	}
+
+	.button svg {
+		height: 20px;
+	}
+
+	.button path {
+		fill: white;
 	}
 
 	@media (max-width: 869px) {
@@ -124,9 +195,7 @@
 		.image {
 			display: none;
 		}
-		.reviewsWrap > div:first-child {
-			padding: 50px;
-		}
+
 		h3 {
 			text-shadow: 2px 2px 10px black;
 		}
