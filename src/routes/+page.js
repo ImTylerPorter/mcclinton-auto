fc13345685e27fcf341f8f022a3fd159295b865a
Tyler Porter <hello@cleverladder.com>
2024-11-27 19:45:26 -0800

    format

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,36 +2,34 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 export async function load() {
-  try {
-    const url = 'https://www.carwise.com/auto-body-shops/shopPlugin?rfid=555006&re=1&type=f&theme=&size=m&zip=&demo=f&ap=false';
-    const { data } = await axios.get(url);
+	try {
+		const url =
+			'https://www.carwise.com/auto-body-shops/shopPlugin?rfid=555006&re=1&type=f&theme=&size=m&zip=&demo=f&ap=false';
+		const { data } = await axios.get(url);
 
-    const $ = cheerio.load(data);
-    let reviews = [];
+		const $ = cheerio.load(data);
+		let reviews = [];
 
-    $('#reviewList li.reviewListItem').each((i, elem) => {
-      const comment = $(elem).find('.comment').text().trim();
-      const customerName = $(elem).find('.customerName').text().trim();
-      const carMake = $(elem).find('.carMake').text().trim();
-      if (comment !== '') {
-        reviews.push({
-          comment,
-          customerName,
-          carMake,
-        });
-      }
+		$('#reviewList li.reviewListItem').each((i, elem) => {
+			const comment = $(elem).find('.comment').text().trim();
+			const customerName = $(elem).find('.customerName').text().trim();
+			const carMake = $(elem).find('.carMake').text().trim();
+			if (comment !== '') {
+				reviews.push({
+					comment,
+					customerName,
+					carMake
+				});
+			}
+		});
 
-    });
-
-    return {
-      reviews
-    };
-  } catch (error) {
-    console.error('Error scraping the website:', error);
-    return {
-      error: 'Failed to scrape the website'
-    };
-  }
+		return {
+			reviews
+		};
+	} catch (error) {
+		console.error('Error scraping the website:', error);
+		return {
+			error: 'Failed to scrape the website'
+		};
+	}
 }
-
-
